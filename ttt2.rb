70c8c5b078c691d0d16068bdfd6600964a5b40c7
Steven Shen <steeve.shen@gmail.com>
2017-08-13 19:09:06 -0400

    v2.1

--- ttt2.rb
+++ ttt2.rb
@@ -37,7 +37,6 @@ class Computer < Player
 end
 
 class Board
-  INDENT = ' ' * 40
   UNMARKED = ' '
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], # horizontal
                    [1, 4, 7], [2, 5, 8], [3, 6, 9], # vertical
